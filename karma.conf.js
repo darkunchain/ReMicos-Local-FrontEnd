faadd0ee36f6b53663918aa1cbe455e30c869b0a
darkunchain <luisfer74081@gmail.com>
2022-01-27 12:23:14 -0500

    ng build fin

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/re-micos-local-front-end'),
+      dir: require('path').join(__dirname, './coverage/remicos-local-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
